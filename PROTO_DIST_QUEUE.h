29450d8c0f91394338b8edb960e77b9521139acf
lucascalam <lucascalambrin@gmail.com>
2021-06-02 09:47:16 -0300

    Updating PROTO_DIST_QUEUE

--- PROTO_DIST_QUEUE.h
+++ PROTO_DIST_QUEUE.h
@@ -7,7 +7,8 @@
 #include <arpa/inet.h>
 
 #define SZ_PATH 256
-#define MAX_ARGS 20
+#define NUM_PATH 1
+#define ARG_MAX 20
 
 typedef enum {
     VER_1 = 0x8001,
@@ -25,6 +26,8 @@ typedef enum {
     TYPE_UNSUB_RESP,                    //main_server to cli
     TYPE_SENDJOB_NOINTER,               //main_server to exec_server
     TYPE_SENDJOB_INTER,                 //main_server to exec_server
+    TYPE_CH_EXEC_SER,                   //main_server to exec_server
+    TYPE_CH_EXEC_SER_ACK,               //exec_server to main_server
     TYPE_JOB_REQ,                       //exec_server to main_server
     TYPE_JOB_DONE,                      //exec_server to main_server
     TYPE_JOB_RUN,                       //exec_server to main_server
@@ -32,6 +35,7 @@ typedef enum {
     TYPE_CLIENT_CLOSED,                 //cli to main_server
     TYPE_SERVER_CLOSED,                 //exec_server to main_server
     TYPE_BAD_PARAMS                     //main_server to cli
+
 } Type;
 
 typedef struct __attribute__((__packed__)) 
@@ -43,7 +47,7 @@ typedef struct __attribute__((__packed__))
 
 typedef struct __attribute__((__packed__)) 
 {
-    char exec_file[SZ_PATH * MAX_ARGS];
+    char exec_file[SZ_PATH * NUM_PATH];
     char working_dir[SZ_PATH];
 } File_Exec;
 
@@ -54,21 +58,17 @@ typedef struct __attribute__((__packed__))
 
 typedef struct __attribute__((__packed__)) 
 {
-    uint16_t queue_id;
+    uint8_t queue_id;
 
 } QueueID;
 
 typedef enum {
-    STATE_WAIT,        
+    STATE_WAIT,
     STATE_RUN,         
     STATE_FINISHED,     
     STATE_UNSUBMITED,
     STATE_SIGNALED,
-    // STATE_WAIT_INTER,        
-    // STATE_RUN_INTER,         
-    // STATE_FINISHED_INTER,     
-    // STATE_UNSUBMITED_INTER,
-    // STATE_SIGNALED_INTER,
+    STATE_ERROR,
 } JOB_STATE;
 
 typedef enum {
@@ -116,7 +116,7 @@ typedef struct __attribute__((__packed__))
     } payload;
 } Msg;
 
-//inline is for simple functions
+/***** Getters - Header *****/
 inline static uint16_t getVersion(const Header *hdr)
 {
     return ntohs(hdr->version);
@@ -127,14 +127,123 @@ inline static uint16_t getSize8(const Header *hdr)
     return ntohs(hdr->sz8);
 }
 
+inline static uint8_t getType(const Header *hdr)
+{
+    return hdr->type;
+}
+
+/***** Getters - Payload *****/
+inline static uint16_t getExecAddr_jid(const Msg *pkg)
+{   
+    return ntohs(pkg->payload.job_exec_addr.jobid.job_id);
+}
+
+inline static uint32_t getExecAddr_ipadd(const Msg *pkg)
+{   
+    return pkg->payload.job_exec_addr.ipaddr;
+}
+
+inline static uint16_t getExecAddr_port(const Msg *pkg)
+{   
+    return pkg->payload.job_exec_addr.port;
+}
+
+inline static const File_Exec *getExecAddr_fileExec(const Msg *pkg)
+{   
+    return &pkg->payload.job_exec_addr.file_exec;
+}
+
+inline static const char *getExecAddr_fileExec_execfile(const Msg *pkg)
+{   
+    return pkg->payload.job_exec_addr.file_exec.exec_file;
+}
+
+inline static const char *getExecAddr_fileExec_wdir(const Msg *pkg)
+{   
+    return pkg->payload.job_exec_addr.file_exec.working_dir;
+}
+
+inline static uint16_t getExec_jid(const Msg *pkg)
+{   
+    return ntohs(pkg->payload.job_exec.jobid.job_id);
+}
+
+inline static const File_Exec *getExec_fileExec(const Msg *pkg)
+{   
+    return &pkg->payload.job_exec.file_exec;
+}
+
+inline static const char *getExec_fileExec_execfile(const Msg *pkg)
+{   
+    return pkg->payload.job_exec.file_exec.exec_file;
+}
+
+inline static const char *getExec_fileExec_wdir(const Msg *pkg)
+{   
+    return pkg->payload.job_exec.file_exec.working_dir;
+}
+
+inline static uint16_t getJobID(const Msg *pkg)
+{   
+    return ntohs(pkg->payload.jobid.job_id);
+}
+
+inline static uint16_t getJobID_NoIter(const Msg *pkg)
+{   
+    return ntohs(pkg->payload.job_exec.jobid.job_id);
+}
+
+inline static uint16_t getJobID_Iter(const Msg *pkg)
+{   
+    return ntohs(pkg->payload.job_exec_addr.jobid.job_id);
+}
+
+inline static QueueID getQueueID(const Msg *pkg)
+{   
+    return pkg->payload.queueid;
+}
+
+inline static uint16_t getJobState_ID(const Msg *pkg)
+{
+    return ntohs(pkg->payload.job_state.jobid.job_id);
+}
+
+inline static uint8_t getJobState_State(const Msg *pkg)
+{
+    return pkg->payload.job_state.state;
+}
+
+inline static uint8_t getJobState_ExitValue(const Msg *pkg)
+{
+    return pkg->payload.job_state.exit_value;
+}
+
+inline static uint8_t getQueueState_ID(const Msg *pkg)
+{
+    return pkg->payload.queue_state.queueid.queue_id;
+}
+
+inline static uint8_t getQueueState_State(const Msg *pkg)
+{
+    return pkg->payload.queue_state.state;
+}
+
+void printType(const Msg *m);
+int sendMsg(int sockfd, const Msg *msg);
+int recvMsg(int sockfd, Msg *msg);
+
+/***** Setters *****/
+
+/**** Main Server ****/
+
 inline static void setFileExecNoInter(Msg *msg, char file_ex[], char working_dir[], uint16_t job_id)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_SENDJOB_NOINTER;
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID) + sizeof(File_Exec));
-    strncpy(msg->payload.job_exec.file_exec.exec_file,file_ex,SZ_PATH*MAX_ARGS);
+    strncpy(msg->payload.job_exec.file_exec.exec_file,file_ex,SZ_PATH * NUM_PATH);
     strncpy(msg->payload.job_exec.file_exec.working_dir,working_dir,SZ_PATH);
-    msg->payload.job_exec.jobid.job_id = job_id;
+    msg->payload.job_exec.jobid.job_id = htons(job_id);
 }
 
 inline static void setFileExecInter(Msg *msg, char file_ex[], char working_dir[],struct  in_addr addr,in_port_t port, uint16_t job_id)
@@ -142,34 +251,11 @@ inline static void setFileExecInter(Msg *msg, char file_ex[], char working_dir[]
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_SENDJOB_INTER;
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID) + sizeof(uint32_t) + sizeof(uint16_t) + sizeof(File_Exec));
-    strncpy(msg->payload.job_exec_addr.file_exec.exec_file,file_ex,SZ_PATH*MAX_ARGS);
+    strncpy(msg->payload.job_exec_addr.file_exec.exec_file,file_ex,SZ_PATH * NUM_PATH);
     strncpy(msg->payload.job_exec_addr.file_exec.working_dir,working_dir,SZ_PATH);
-    msg->payload.job_exec_addr.jobid.job_id = job_id;
+    msg->payload.job_exec_addr.jobid.job_id = htons(job_id);
     msg->payload.job_exec_addr.ipaddr = (uint32_t) addr.s_addr;
     msg->payload.job_exec_addr.port = (uint16_t)port;
-}
-
-inline static void setFileExecNoInter_CLIENT(Msg *msg, char file_ex[], char working_dir[], uint16_t job_id)
-{   
-    msg->hdr.version = htons(VER_1);
-    msg->hdr.type = TYPE_SUB_NO_INTER;
-    msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID) + sizeof(File_Exec));
-    strncpy(msg->payload.job_exec.file_exec.exec_file,file_ex, SZ_PATH*MAX_ARGS);
-    strncpy(msg->payload.job_exec.file_exec.working_dir,working_dir, SZ_PATH);
-    msg->payload.job_exec.jobid.job_id = job_id;
-}
-
-inline static void setFileExecInter_CLIENT(Msg *msg, char file_ex[], char working_dir[],struct  in_addr addr,in_port_t port, uint16_t job_id)
-{   
-    msg->hdr.version = htons(VER_1);
-    msg->hdr.type = TYPE_SUB_INTERACTIVE;
-    msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID) + sizeof(uint32_t) + sizeof(uint16_t) + sizeof(File_Exec));
-    strncpy(msg->payload.job_exec_addr.file_exec.exec_file,file_ex,SZ_PATH*MAX_ARGS);
-    strncpy(msg->payload.job_exec_addr.file_exec.working_dir,working_dir,SZ_PATH);
-    msg->payload.job_exec_addr.jobid.job_id = job_id;
-    msg->payload.job_exec_addr.ipaddr = (uint32_t) addr.s_addr;
-    msg->payload.job_exec_addr.port = (uint16_t)port;
-
 }
 
 inline static void setJobState(Msg *msg, Job_State job_state)
@@ -177,7 +263,7 @@ inline static void setJobState(Msg *msg, Job_State job_state)
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_JOBSTATE_RESP;
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(Job_State));
-    msg->payload.job_state.jobid = job_state.jobid;
+    msg->payload.job_state.jobid.job_id = htons(job_state.jobid.job_id);
     msg->payload.job_state.state = job_state.state;
     msg->payload.job_state.exit_value = job_state.exit_value;
 }
@@ -191,11 +277,11 @@ inline static void setQueueState(Msg *msg, Queue_State queue_state)
     msg->payload.queue_state.state = queue_state.state;
 }
 
-inline static void setUnsubmit(Msg *msg, JobID jobid)
+inline static void setUnsubmit(Msg *msg, uint16_t jobid)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_UNSUB_RESP;
-    msg->payload.jobid = jobid;
+    msg->payload.jobid.job_id = htons(jobid);
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID));
 }
 
@@ -206,20 +292,37 @@ inline static void setBadParams(Msg *msg)
     msg->hdr.sz8 = htons(sizeof(Header));
 }
 
-inline static JobID getJobID(Msg *pkg)
+inline static void setCheckExecServer(Msg *msg)
 {   
-    return pkg->payload.jobid;
+    msg->hdr.version = htons(VER_1);
+    msg->hdr.type = TYPE_CH_EXEC_SER;
+    msg->hdr.sz8 = htons(sizeof(Header));
 }
 
-inline static Job_State getJobState(Msg *pkg){
-    return pkg->payload.job_state;
+/**** CLIENT ****/
+
+inline static void setFileExecNoInter_CLIENT(Msg *msg, char file_ex[], char working_dir[], uint16_t job_id)
+{   
+    msg->hdr.version = htons(VER_1);
+    msg->hdr.type = TYPE_SUB_NO_INTER;
+    msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID) + sizeof(File_Exec));
+    strncpy(msg->payload.job_exec.file_exec.exec_file,file_ex, SZ_PATH * NUM_PATH);
+    strncpy(msg->payload.job_exec.file_exec.working_dir,working_dir, SZ_PATH);
+    msg->payload.job_exec.jobid.job_id = htons(job_id);
 }
 
-inline static Queue_State getQueueState(Msg *pkg){
-    return pkg->payload.queue_state;
-}
+inline static void setFileExecInter_CLIENT(Msg *msg, char file_ex[], char working_dir[],struct  in_addr addr,in_port_t port, uint16_t job_id)
+{   
+    msg->hdr.version = htons(VER_1);
+    msg->hdr.type = TYPE_SUB_INTERACTIVE;
+    msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID) + sizeof(uint32_t) + sizeof(uint16_t) + sizeof(File_Exec));
+    strncpy(msg->payload.job_exec_addr.file_exec.exec_file,file_ex,SZ_PATH * NUM_PATH);
+    strncpy(msg->payload.job_exec_addr.file_exec.working_dir,working_dir,SZ_PATH);
+    msg->payload.job_exec_addr.jobid.job_id = htons(job_id);
+    msg->payload.job_exec_addr.ipaddr = (uint32_t) addr.s_addr;
+    msg->payload.job_exec_addr.port = (uint16_t)port;
 
-/***** CLIENT *****/
+}
 
 inline static void setClosedConnection(Msg *msg)
 {   
@@ -232,7 +335,7 @@ inline static void setJobID(Msg *msg, JobID jobid)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_SUB_RESP;
-    msg->payload.jobid = jobid;
+    msg->payload.jobid.job_id = htons(jobid.job_id);
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID));
 }
 
@@ -240,7 +343,7 @@ inline static void setJobState_CLIENT(Msg *msg, JobID jobid)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_JOBSTATE;
-    msg->payload.jobid = jobid;
+    msg->payload.jobid.job_id = htons(jobid.job_id);
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID));
 }
 
@@ -256,15 +359,11 @@ inline static void setUnsubmit_CLIENT(Msg *msg, JobID jobid)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_UNSUB;
-    msg->payload.jobid = jobid;
+    msg->payload.jobid.job_id = htons(jobid.job_id);
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(JobID));
 }
 
-void printType(const Msg *m);
-int sendMsg(int sockfd, const Msg *msg);
-int recvMsg(int sockfd, Msg *msg);
-
-/***** exec_server *****/
+/**** exec_server ****/
 
 inline static void setClosedConnection_ExecServer(Msg *msg)
 {   
@@ -280,34 +379,47 @@ inline static void setJobReq(Msg *msg)
     msg->hdr.sz8 = htons(sizeof(Header));
 }
 
-inline static void setJobRun(Msg *msg, JobID jobid)
+inline static void setJobRun(Msg *msg, uint16_t job_id)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_JOB_RUN;
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(Job_State));
-    msg->payload.job_state.jobid.job_id = jobid.job_id;
+    msg->payload.job_state.jobid.job_id = htons(job_id);
     msg->payload.job_state.state = STATE_RUN;
     msg->payload.job_state.exit_value = 0;
 }
 
-inline static void setJobDone(Msg *msg, JobID jobid, uint8_t e_status)
+inline static void setJobDone(Msg *msg, uint16_t job_id, uint8_t e_status)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_JOB_DONE;
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(Job_State));
-    msg->payload.job_state.jobid.job_id = jobid.job_id;
-    msg->payload.job_state.state = STATE_FINISHED;
+    msg->payload.job_state.jobid.job_id = htons(job_id);
+
+    if ( e_status == 40 || e_status == 41){
+        msg->payload.job_state.state = STATE_ERROR;
+    } else { 
+        msg->payload.job_state.state = STATE_FINISHED;
+    }
+
     msg->payload.job_state.exit_value = e_status;
 }
 
-inline static void setJobSignaled(Msg *msg, JobID jobid, uint8_t e_status)
+inline static void setJobSignaled(Msg *msg, uint16_t job_id, uint8_t e_status)
 {   
     msg->hdr.version = htons(VER_1);
     msg->hdr.type = TYPE_JOB_SIG;
     msg->hdr.sz8 = htons(sizeof(Header) + sizeof(Job_State));
-    msg->payload.job_state.jobid.job_id = jobid.job_id;
+    msg->payload.job_state.jobid.job_id = htons(job_id);
     msg->payload.job_state.state = STATE_SIGNALED;
     msg->payload.job_state.exit_value = e_status;
+}
+
+inline static void setCheckExecServerAck(Msg *msg)
+{   
+    msg->hdr.version = htons(VER_1);
+    msg->hdr.type = TYPE_CH_EXEC_SER_ACK;
+    msg->hdr.sz8 = htons(sizeof(Header));
 }
 
 #endif // PROTO_DIST_QUEUE_H_
